5e70f5d0774f417c3301e200bab58218623e4e67
mthomsit <mark@makeandship.co.uk>
2020-01-27 17:17:48 +0000

    Zip all base themes

--- deploy/theme-entrypoint.sh
+++ deploy/theme-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e 
 
-ls -la /app/themes/
-
 cp -R /app/themes/base /app/dist/
 cp -R /app/themes/common /app/dist/
 cp -R /app/themes/keycloak /app/dist/
 cp -R /app/themes/keycloak-preview /app/dist/
 cp -R /app/themes/mykrobe-atlas /app/dist/
 
-ls -la /app/dist/
-
 cd /app/dist/ && zip -r ./mykrobe-atlas-themes.zip ./
+
+ls -la /app/dist/
