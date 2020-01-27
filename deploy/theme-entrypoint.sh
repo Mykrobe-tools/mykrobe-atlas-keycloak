b7d47fd7a2179e2b595d454445c04d5ddabde008
mthomsit <mark@makeandship.co.uk>
2020-01-27 16:25:20 +0000

    Explicitely copy themes

--- deploy/theme-entrypoint.sh
+++ deploy/theme-entrypoint.sh
@@ -3,7 +3,11 @@ set -e
 
 ls -la /app/themes/
 
-cp -R /app/themes/ /app/dist/
+cp -R /app/themes/base /app/dist/
+cp -R /app/themes/common /app/dist/
+cp -R /app/themes/keycloak /app/dist/
+cp -R /app/themes/keycloak-preview /app/dist/
+cp -R /app/themes/mykrobe-atlas /app/dist/
 
 ls -la /app/dist/
 
