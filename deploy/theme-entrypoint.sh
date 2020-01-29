6bb118ceb42e0e276229ceefc965534322b243f2
mthomsit <mark@makeandship.co.uk>
2020-01-29 13:22:18 +0000

    Adjust zip

--- deploy/theme-entrypoint.sh
+++ deploy/theme-entrypoint.sh
@@ -7,6 +7,6 @@ cp -R /app/themes/keycloak /app/dist/
 cp -R /app/themes/keycloak-preview /app/dist/
 cp -R /app/themes/mykrobe-atlas /app/dist/
 
-cd /app/dist/ && tar -cvzf mykrobe-atlas-themes.tar.gz ./ 
+cd /app/dist/ && tar -cvzf mykrobe-atlas-themes.tar.gz ./base ./common ./keycloak ./keycloak-preview ./mykrobe-atlas
 
 ls -la /app/dist/
