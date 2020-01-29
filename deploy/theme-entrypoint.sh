70257a9ab531a736ad65ef060d97bde589b1fb4e
mthomsit <mark@makeandship.co.uk>
2020-01-29 11:24:22 +0000

    Use tar and gzip for compression

--- deploy/theme-entrypoint.sh
+++ deploy/theme-entrypoint.sh
@@ -7,6 +7,6 @@ cp -R /app/themes/keycloak /app/dist/
 cp -R /app/themes/keycloak-preview /app/dist/
 cp -R /app/themes/mykrobe-atlas /app/dist/
 
-cd /app/dist/ && zip -r ./mykrobe-atlas-themes.zip ./
+cd /app/dist/ && tar -cv ./ | gzip > mykrobe-atlas-themes.tar.gz
 
 ls -la /app/dist/
