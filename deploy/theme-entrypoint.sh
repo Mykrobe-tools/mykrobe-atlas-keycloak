dbf4b75f04a3269a2256fd8a64e5a356661aa912
Yassire Elhani <yassire@makeandship.com>
2020-04-30 15:35:41 +0100

    remove base themes

--- deploy/theme-entrypoint.sh
+++ deploy/theme-entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e 
 
+#cp -R /app/themes/base /app/dist/
+#cp -R /app/themes/common /app/dist/
+#cp -R /app/themes/keycloak /app/dist/
+#cp -R /app/themes/keycloak-preview /app/dist/
 cp -R /app/themes/mykrobe-atlas /app/dist/
 
+#cd /app/dist/ && tar -cvzf mykrobe-atlas-themes.tar.gz ./base ./common ./keycloak ./keycloak-preview ./mykrobe-atlas
 cd /app/dist/ && tar -cvzf mykrobe-atlas-themes.tar.gz ./mykrobe-atlas
 
 ls -la /app/dist/
